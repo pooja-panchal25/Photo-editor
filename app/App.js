43e4580747464d6024bb6796f6ba3183a04143b6
Pooja Panchal <poojapanchal2502@gmail.com>
2025-10-07 12:52:17 +0530

    Deleted

--- app/App.js
+++ app/App.js
@@ -17,8 +17,6 @@ const App = () => {
   const remoteURL =
     'https://images.unsplash.com/photo-1634915728822-5ad85582837a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80';
 
-
-
   const onEdit = async () => {
     try {
       const path = await PhotoEditor.open({
